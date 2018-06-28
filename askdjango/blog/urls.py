948e459e6479cb10f6f526c78b05df76d645219c
amazing-man <37244101+hagull@users.noreply.github.com>
2018-06-28 19:00:57 +0900

    정규표현식 예제 및 url 설명

--- askdjango/blog/urls.py
+++ askdjango/blog/urls.py
@@ -1,21 +1,10 @@
-"""askdjango URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
+#blog/urls.py
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
 ]
+'''
+첫번째 값은 blog이후에 값이 X
+'''
