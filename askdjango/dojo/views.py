9ca5d5b994b35089fe5ee9586356903fd1305483
amazing-man <37244101+hagull@users.noreply.github.com>
2018-06-28 19:40:29 +0900

    url 다중으로 받기

--- askdjango/dojo/views.py
+++ askdjango/dojo/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def mysum(request, x, y = 0, z = 0):
+def mysum(request, numbers):
     #request:httprquest
-    return HttpResponse(int(x) + int(y) + int(z))
+    #numbers = "1/2/12/123/12312/
+    result = sum(map(int, numbers.split("/")))
+    # 위코드는
+    # split1 = numbers.split("/")
+    # for i in range(len(split1)):
+    #   result += split1[i]
+    #와 같다
+    #sum은 변수지정이 안되네 함수명으로 있어서 그런듯
+    return HttpResponse(result)
