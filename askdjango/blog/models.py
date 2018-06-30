3bea02a0154cf4c128d669a32ec033159838b44c
amazing-man <37244101+hagull@users.noreply.github.com>
2018-06-30 19:45:04 +0900

    장고 extensinos 을 이용한 주피터 노트북환경에서 쉘 구성 및 admin

--- askdjango/blog/models.py
+++ askdjango/blog/models.py
@@ -9,6 +9,8 @@ def lnglat_validator(value):
         raise ValidationError('Invalid LngLat Type')
 
 class Post(models.Model):
+    author = models.CharField(max_length=20)
+    #blank 옵션을 주지않으면 필수필드이다
     title = models.CharField(max_length=100, verbose_name = '제목', help_text = '포스팅 제목을 입력해주세요. 최대 100자') # 길이 제한이 있는 문자열
     content = models.TextField(verbose_name= '내용')#길이 제한이 없는 문자열
     tags = models.CharField(max_length=100, blank=True)
