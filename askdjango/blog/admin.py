047c8cfd0610b37d13357853b7764eb142dde0f2
amazing-man <37244101+hagull@users.noreply.github.com>
2018-06-30 21:04:24 +0900

    admin action 등등 설정

--- askdjango/blog/admin.py
+++ askdjango/blog/admin.py
@@ -6,10 +6,31 @@ from django.utils.safestring import mark_safe
 #기본적으로 태그를 escape 해준다 장고는 즉 위의 함수를 임포트 하여 허용해야한다
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'content_size', 'created_at', 'updated_at']
+    list_display = ['id', 'title', 'content_size','status', 'created_at', 'updated_at']
+    actions = ['make_published', 'make_draft', 'make_withdrawn']
+
     def content_size(self, post):
         return mark_safe('<strong>{}글자</strong>'.format(len(post.content)))
     content_size.short_description = '내용 글자수'
+
+    def make_published(self, request, queryset):
+        updated_count = queryset.update(status='p') #QuerySet.update
+        #자동으로 카운트 되는건가 ?
+        self.message_user(request, '{}건의 포스팅을 Published상태로 변경'.format(updated_count)) #django 메세지 프레임워크 활용
+    make_published.short_description = '지정 포스팅을 Published상태로 변경합니다'
+
+    def make_draft(self, request, queryset):
+        updated_count = queryset.update(status='d') #QuerySet.update
+        #자동으로 카운트 되는건가 ?
+        self.message_user(request, '{}건의 포스팅을 Draft상태로 변경'.format(updated_count)) #django 메세지 프레임워크 활용
+    make_draft.short_description = '지정 포스팅을 Draft상태로 변경합니다'
+
+    def make_withdrawn(self, request, queryset):
+        updated_count = queryset.update(status='w') #QuerySet.update
+        #자동으로 카운트 되는건가 ?
+        self.message_user(request, '{}건의 포스팅을 Withdrawn상태로 변경'.format(updated_count)) #django 메세지 프레임워크 활용
+    make_withdrawn.short_description = '지정 포스팅을 Withdrawn상태로 변경합니다'
+
     #위의 묘사를 다르게해줌 모델에서 지정한 verbose_name = '제목' 와 같이
     #content_size.allow_tags = True
     #1.9 부터 삭제 1.9부터는 django mark_safe라는 함수를 이용
--- askdjango/blog/models.py
+++ askdjango/blog/models.py
@@ -9,6 +9,11 @@ def lnglat_validator(value):
         raise ValidationError('Invalid LngLat Type')
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdrawn'),
+    )
     author = models.CharField(max_length=20)
     #blank 옵션을 주지않으면 필수필드이다
     title = models.CharField(max_length=100, verbose_name = '제목', help_text = '포스팅 제목을 입력해주세요. 최대 100자') # 길이 제한이 있는 문자열
@@ -19,4 +24,5 @@ class Post(models.Model):
                               validators=[lnglat_validator])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=1, choices= STATUS_CHOICES)
 # Create your models here.
