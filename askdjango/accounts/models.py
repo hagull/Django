e1b32999680fb28fa78f1f553085f9fc0ec26f4b
amazing-man <37244101+hagull@users.noreply.github.com>
2018-07-03 19:56:33 +0900

    accounts 에 유저모델 1:1 프로필 매칭 과 User모델의 올바른 지정

--- askdjango/accounts/models.py
+++ askdjango/accounts/models.py
@@ -1,9 +1,12 @@
 # accounts/models.py
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 # Create your models here.
 class Profile(models.Model):
 #    user = models.ForeignKey('auth.User') BAD CASE
-    user = models.OneToOneField(User)
+#    user = models.OneToOneField(User) # BAD CASE
+#    user = models.OneToOneField('auth.User') BADCASE
+    user = models.OneToOneField(settings.AUTH_USER_MODEL) # GOOD CASE
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
