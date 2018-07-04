a107acd3fa5d2441b68a7675978516403a6d5abe
amazing-man <37244101+hagull@users.noreply.github.com>
2018-07-04 21:07:50 +0900

    각각의 app마다 namespace 적용

--- askdjango/askdjango/urls.py
+++ askdjango/askdjango/urls.py
@@ -16,12 +16,22 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
+from django.shortcuts import redirect
+from django.views.generic import RedirectView
+def root(request):
+    return redirect('blog:post_list')
+
 urlpatterns = [
+    #url(r'^$', root, name = 'root'),
+    # 함수 지정을 따로 안하고 lambda 함수로도 지정이 가능
+    url(r'^$', lambda r: redirect('blog:post_list'), name = 'root'),
+    #람다 함수는 함수의 축약형 ex) g = lambda x,y : x * y // g(2,3) = 6 이런식으로 이것도 파이썬 문법
+    #url(r'^$', RedirectView.as_view(pattern_name= 'blog:post_list')), 로도 지정가능
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls')),
-    url(r'^dojo/', include("dojo.urls")),
-    url(r'^accounts/', include("accounts.urls")),
-    url(r'^shop/', include('shop.urls')),
+    url(r'^blog/', include('blog.urls', namespace= 'blog')),
+    url(r'^dojo/', include("dojo.urls", namespace= "dojo")),
+    url(r'^accounts/', include("accounts.urls", namespace="accounts")),
+    url(r'^shop/', include('shop.urls', namespace="shop")),
 ]
 # 실 서비스 에서는 setting.py의 DEBUG 옵션은 False 로 둔다 TRUE는 개발옵션을 킨다는 뜻
 if settings.DEBUG:
